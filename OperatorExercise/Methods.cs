9d33df12b0e7112611bf656aba07f9a0c54796a4
Jacob Phillips <jakephillips.wv@gmail.com>
2022-09-12 21:19:14 -0500

    initial commit

---
+++ OperatorExercise/Methods.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorExercise
+{
+    internal class Methods
+    {
+        public static void AreaOfCircle()
+        {
+            Console.WriteLine("What is the radius of your circle?");
+            double radius = double.Parse(Console.ReadLine());
+            double area = Math.PI * Math.Pow(radius, 2);
+            Console.WriteLine(area);
+        }
+    }
+}
---
+++ OperatorExercise/Program.cs
@@ -0,0 +1,39 @@
+﻿using OperatorExercise;
+
+Console.WriteLine("--------------------------------Exercise 1------------------------------------");
+Console.WriteLine();
+
+int a = 17;
+int b = 4;
+int quotient = a / b;
+int remainder = a % b;
+
+if (a == 17 && b == 4)
+{
+    Console.WriteLine($"{a}/{b} is {quotient} remainder {remainder}");
+}
+else
+{
+    Console.WriteLine("False");
+}
+
+
+Console.WriteLine();
+Console.WriteLine("--------------------------------Exercise 2------------------------------------");
+Console.WriteLine();
+
+Methods.AreaOfCircle();
+
+Console.WriteLine();
+Console.WriteLine("--------------------------------Thought Exercise------------------------------------");
+Console.WriteLine();
+
+//The equation below outputs 16. The ++i will calculate before k is calcuated and therefore its value is 3+1=4. The j++ will not
+//calculate until after k calculates (for the next time) and therefore its value remains 4. That will make k's calculation become 
+//4*4=16
+
+var i = 3;
+var j = 4;
+var k = ++i * j++;
+
+Console.WriteLine(k);
